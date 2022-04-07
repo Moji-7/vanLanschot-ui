165f36c2737e7c461c75a644ad61a20e57b8fa31
Moji <mirzaei_7@yahoo.com>
2022-04-08 01:17:43 +0430

    Delete src/app/admin/dynamic-form directory

--- src/app/admin/dynamic-form/create/services/question-checkbox.ts
+++
@@ -1,6 +0,0 @@
-import { QuestionBase } from './question-base';
-
-export class CheckboxQuestion extends QuestionBase<Boolean> {
-  override controlType = 'checkbox';
-  override value: Boolean|undefined ;
-}
